32ac29ec9b7aed2ec858a1b7bd3238b26c269bec
@THIYAGUSRI <thiyagusrinivasn09@gmail.com>
2024-08-06 00:14:39 +0530

    Update

--- API/index.js
+++ API/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() =>{ console.log('MongoDB is connected')}).catch((err) => {console.log(err)});
-const app=express();
 
+const app=express();
+app.use(express.json);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.use('/API/user', userRoutes); 
+app.use('/api/user', userRoutes); 
+app.use('api/auth', authRoutes);
