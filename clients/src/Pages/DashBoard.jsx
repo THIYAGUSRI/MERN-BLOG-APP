1fd9b847c48193be7f9686028e483be6227edc24
@THIYAGUSRI <thiyagusrinivasan09@gmail.com>
2024-08-19 23:23:07 +0530

    Update the sliderbar of the dashboard

--- clients/src/Pages/DashBoard.jsx
+++ clients/src/Pages/DashBoard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../Components/DashSidebar';
+import DashProfile from '../Components/DashProfile';
 
 export default function DashBoard() {
+  const location = useLocation()
+  const [ tab, setTab ] = useState('')
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search);
+    const tabFromUrl = urlparams.get('tab');
+    if(tabFromUrl)
+    {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>DashBoard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sliebar*/}
+        <DashSidebar />
+      </div>
+      <div className=''>
+        {/* profile ...*/}
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
   )
 }
