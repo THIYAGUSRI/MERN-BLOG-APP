17f78beb74eb51591348a48f04364ad5392449aa
@THIYAGUSRI <thiyagusrinivasan09@gmail.com>
2024-08-24 23:53:35 +0530

    Update the code

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,31 +1,30 @@
 import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
-
 export const create = async (req, res, next) => {
-    if (!req.user.isAdmin) {
-        return next(errorHandler(403, 'You are not allowed to create a post'));
-      }
-      if (!req.body.title || !req.body.content) {
-        return next(errorHandler(400, 'Please provide all required fields'));
-      }
-      const slug = req.body.title
-        .split(' ')
-        .join('-')
-        .toLowerCase()
-        .replace(/[^a-zA-Z0-9-]/g, '');
-      const newPost = new Post({
-        ...req.body,
-        slug,
-        userId: req.user.id,
-      });
-      try {
-        const savedPost = await newPost.save();
-        res.status(201).json(savedPost);
-      } catch (error) {
-        next(error);
-      }
-}; 
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to create a post'));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, 'Please provide all required fields'));
+  }
+  const slug = req.body.title
+    .split(' ')
+    .join('-')
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, '');
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getposts = async (req, res, next) => {
   try {
@@ -70,7 +69,7 @@ export const getposts = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}; 
+};
 
 export const deletepost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
@@ -106,4 +105,3 @@ export const updatepost = async (req, res, next) => {
     next(error);
   }
 };
-
